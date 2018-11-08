8e0102965dc6e604ff3a5a4e0aa629cafc724165
Zaven <matthew.fullstackgis@gmail.com>
2018-11-08 14:32:02 -0800

    Move all dev container stuff to dev/

--- container/build.sh
+++
@@ -1,4 +0,0 @@
-docker build . -f container/Dockerfile.ReverseProxy -t treetracker-reverse-proxy:latest
-docker build . -f container/Dockerfile.Web -t treetracker-admin:latest
-docker build . -f container/Dockerfile.API -t treetracker-admin-api:latest
-
---
+++ dev/build.sh
@@ -0,0 +1,4 @@
+docker build . -f dev/Dockerfile.ReverseProxy -t treetracker-reverse-proxy:latest
+docker build . -f dev/Dockerfile.Web -t treetracker-admin-web:latest
+docker build . -f dev/Dockerfile.API -t treetracker-admin-api:latest
+
