98e0ab09f2be2f10b7c7259bf631a4697c87abb3
Mitul Patel <mtlpatel@protonmail.com>
2018-12-05 20:48:27 -0500

    GET Request tests

--- test/trees.test.js
+++ test/trees.test.js
@@ -1,8 +1,40 @@
 const app = require('../server/server.js');
 
 const assert = require('chai').assert;
-const supertest = require('supertest');
+const request = require('supertest');
+
+const trees = app.models.tree;
 
 describe('Trees', () => {
+  it('should get all trees', (done) => {
+    request(app)
+      .get('/Trees')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done());
+  });
 
+  it('should get tree by id', (done) => {
+    request(app)
+      .get('/Trees/6')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done());
+  });
+
+  it('should check if tree exists', (done) => {
+    request(app)
+      .get('/Trees/6/exists')
+      .set('Accept', 'application/json')
+      .expect('Content-type', /json/)
+      .expect(200, done());
+  });
+
+  it('should get the count of trees', (done) => {
+    request(app)
+      .get('/Trees/count')
+      .set('Accept', 'application/json')
+      .expect('Content-type', /json/)
+      .expect(200, done());
+  });
 });
