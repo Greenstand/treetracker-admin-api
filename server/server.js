5d4d5229cb47c3033d23147e73024ffdbe60bff5
Zaven <matthew.fullstackgis@gmail.com>
2019-03-30 16:31:03 -0700

    Moved datasources into config subdir

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, { dsRootDir: 'config/' }, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
