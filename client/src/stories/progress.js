c16405d8235d40fa174e4cc022abd38f301a46b4
deanchen <antlet2004@gmail.com>
2019-08-19 17:38:10 +0800

    a demo for "approve all" function

--- client/src/stories/progress.js
+++ client/src/stories/progress.js
@@ -3,23 +3,41 @@ import { storiesOf } from '@storybook/react';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import AppBar from '@material-ui/core/AppBar';
 import Modal from '@material-ui/core/Modal';
+import * as loglevel		from 'loglevel';
 
+const log		= loglevel.getLogger('../stories/progress')
 
 function TestProgress(){
+		const count		= 100
 		const [isApproving, setApproving] = React.useState(false)
 		const [complete, setComplete]		= React.useState(0)
-		const [pictures, setPictures] = React.useState(Array.from(new Array(10)).map((e,i) => i))
+		const [pictures, setPictures] = React.useState(Array.from(new Array(count)).map((e,i) => i))
+		const [isAllSelected, setAllSelected]		= React.useState(false)
+		//[0, 2] select 0-2
+		const [range, setRange]		= React.useState([-1, -1])
 		console.log('with pictures:%d', pictures.length)
 
 		React.useEffect(() => {
 			console.log('approve...')
 			let timer
-			if(isApproving && pictures.length > 0){
+			if(isApproving && range[0] >= 0 && range[1] >= 0){
 				timer		= setTimeout(() => {
-					pictures.shift()
-					setPictures([...pictures])
+					if(range[0] > range[1]){
+						setRange([-1, -1])
+					}else{
+						//pictures.shift()
+						const picturesNew		= [
+							...pictures.slice(0, range[1] - 1),
+							...pictures.slice(range[1]),
+						]
+						log.log('new list:', picturesNew)
+						setPictures(picturesNew)
+						const rangeNew		= [range[0], range[1] - 1]
+						log.log('range new:', rangeNew)
+						setRange(rangeNew)
+					}
 				}, 300)
-			}else if(isApproving && pictures.length === 0){
+			}else if(isApproving && range[0] === -1 && range[1] === -1){
 				console.log('finished')
 				setApproving(false)
 //				setTimeout(() => {
@@ -29,29 +47,57 @@ function TestProgress(){
 			return () => {
 				clearInterval(timer)
 			}
-		}, [isApproving, pictures])
+		}, [isApproving, pictures, range])
 
 		//set the complete of progress
 		React.useEffect(() => {
-			setComplete((10 - pictures.length) * 10)
+			setComplete((count - pictures.length) * count)
 		}, [pictures])
 
 		function handleApproveAll(){
 			if(window.confirm(
-				`are you sure to approve these ${pictures.length} trees?`,
+				`are you sure to approve these ${range[1] - range[0]} trees?`,
 			)){
 				setApproving(true)
 			}
 		}
 
+		//click picture to select it
+		function handlePictureClick(e, i){
+			console.log('click with:', e.shiftKey)
+			console.log('set range:', i)
+			if(e.shiftKey){
+				setRange([Math.min(i, range[0]), Math.max(i, range[1])])
+			}else{
+				setRange([i, i])
+			}
+		}
+
+		function handleSelectAll(){
+			if(isAllSelected){
+				setRange([-1, -1])
+				setAllSelected(false)
+			}else{
+				setAllSelected(true)
+				setRange([0, count])
+			}
+		}
+
 		return (
 			<div>
-				<h1>tree images</h1>
+				<h1>
+					tree images
+					{range[1] >= 0 && range[0] >= 0 &&
+						<span>({range[1] - range[0] + 1} trees selected)</span>
+					}
+				</h1>
+				<input type='checkbox' checked={isAllSelected} onClick={handleSelectAll} />select all
 				<button onClick={handleApproveAll}>approve all</button>
 				<div
 					style={{
 						display		: 'flex',
 						flexWrap		: 'wrap',
+						userSelect		: 'none',
 					}}
 				>
 					{pictures.map((e,i) => 
@@ -59,9 +105,17 @@ function TestProgress(){
 							style={{
 								width		: 200,
 								height		: 200,
-								border		: '1px solid black',
+								border		: '2px solid black',
+									'2px solid green'
+									:
+									'2px solid black',
+								background		: i >= range[0] && i <= range[1] ?
+									'green'
+									:
+									'white',
 								margin		: 20,
 							}}
+							onClick={(e) => handlePictureClick(e,i)}
 						>
 							PICTURE {e}
 						</div>
