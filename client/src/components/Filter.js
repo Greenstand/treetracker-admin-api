b8710da8fb8e472e49c39377607796288683179a
Nicholas Harris <nharris614@gmail.com>
2019-11-30 19:37:27 -0600

    Added a clear button + some linter fixes

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
 import React, {useState} from 'react'
 import Drawer from '@material-ui/core/Drawer';
 import compose from 'recompose/compose'
@@ -18,172 +20,182 @@ import OutlinedInput		from '@material-ui/core/OutlinedInput';
 import FilterModel		from '../models/Filter'
 import dateformat		from 'dateformat'
 
-export const drawWidth		= 330
+export const drawWidth = 330
 
 const styles = theme => {
-	return {
-	root		: {
-	},
-	drawer: {
-		flexShrink: 0,
-	},
-	drawerPaper: {
-		marginTop		: 64,
-		width: drawWidth,
-		paddingTop		: theme.spacing.unit * 3,
-		paddingLeft		: theme.spacing.unit * 2,
-		paddingRight		: theme.spacing.unit * 2,
-		/*
-		 * boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12)',
-		 * */
-	},
-	close		: {
-		color		: theme.palette.grey[500],
-	},
-	dateInput		: {
-		width		: 158,
-		fontSize		: 14,
-	},
-}}
+  return {
+    root: {
+    },
+    drawer: {
+      flexShrink: 0
+    },
+    drawerPaper: {
+      marginTop: 64,
+      width: drawWidth,
+      paddingTop: theme.spacing.unit * 3,
+      paddingLeft: theme.spacing.unit * 2,
+      paddingRight: theme.spacing.unit * 2
+      /*
+			* boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 5px 8px 0px rgba(0,0,0,0.14), 0px 1px 14px 0px rgba(0,0,0,0.12)',
+			* */
+    },
+    close: {
+      color: theme.palette.grey[500]
+    },
+    dateInput: {
+      width: 158,
+      fontSize: 14
+    }
+  }
+}
 
-function Filter(props){
+function Filter (props) {
+  const { classes, filter }		= props
+  console.error('filter:%o', filter)
+  const dateStartDefault		= '1970-01-01'
+  const dateEndDefault		= `${dateformat(Date.now(), 'yyyy-mm-dd')}`
+  const [treeId, setTreeId]		= useState(filter.treeId)
+  const [status, setStatus]		= useState(filter.status)
+  const [dateStart, setDateStart]		= useState(filter.dateStart || dateStartDefault)
+  const [dateEnd, setDateEnd]		= useState(filter.dateEnd || dateEndDefault)
+  console.error('the tree id:%d', treeId)
 
-	const {classes, filter}		= props
-	console.error('filter:%o', filter)
-	const dateStartDefault		= '1970-01-01'
-	const dateEndDefault		= `${dateformat(Date.now(), 'yyyy-mm-dd')}`
-	const [treeId, setTreeId]		= useState(filter.treeId)
-	const [status, setStatus]		= useState(filter.status)
-	const [dateStart, setDateStart]		= useState(filter.dateStart || dateStartDefault)
-	const [dateEnd, setDateEnd]		= useState(filter.dateEnd || dateEndDefault)
-	console.error('the tree id:%d', treeId)
+  function handleDateStartChange (e) {
+    setDateStart(e.target.value || dateStartDefault)
+  }
 
-	function handleDateStartChange(e){
-		setDateStart(e.target.value || dateStartDefault)
+  function handleDateEndChange (e) {
+    setDateEnd(e.target.value || dateEndDefault)
+  }
+
+  function handleSubmit () {
+    const filter		= new FilterModel()
+    filter.treeId		= treeId
+    filter.status		= status
+    filter.dateStart		= dateStart
+    filter.dateEnd		= dateEnd
+    props.onSubmit && props.onSubmit(filter)
+  }
+
+  function handleClear () {
+		setTreeId('')
 	}
 
-	function handleDateEndChange(e){
-		setDateEnd(e.target.value || dateEndDefault)
-	}
-
-	function handleSubmit(){
-		const filter		= new FilterModel()
-		filter.treeId		= treeId
-		filter.status		= status
-		filter.dateStart		= dateStart
-		filter.dateEnd		= dateEnd
-		props.onSubmit && props.onSubmit(filter)
-	}
-
-	return (
-		<Drawer
-			className={classes.drawer}
-			classes={{
-				paper: classes.drawerPaper,
-			}}
-			open={props.isOpen}
-			variant='persistent'
-			anchor='right'
-			PaperProps={{
-				elevation		: props.isOpen ? 6 : 0,
-			}}
-		>
-			<Grid
-				container
-				justify='space-between'
-			>
-				<Grid item>
-					<Typography variant='h6'>
+  return (
+    <Drawer
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper
+      }}
+      open={props.isOpen}
+      variant='persistent'
+      anchor='right'
+      PaperProps={{
+        elevation: props.isOpen ? 6 : 0
+      }}
+    >
+      <Grid
+        container
+        justify='space-between'
+      >
+        <Grid item>
+          <Typography variant='h6'>
 						Filter
-					</Typography>
-				</Grid>
-				<Grid item>
-					<IconButton 
-						color='primary' 
-						classes={{
-							colorPrimary		: classes.close,
-						}}
-					>
-						<IconClose/>
-					</IconButton>
-				</Grid>
-			</Grid>
-			<Button
-				variant='outlined'
-				color='primary'
-				onClick={handleSubmit}
-			>
+          </Typography>
+        </Grid>
+        <Grid item>
+          <IconButton
+            color='primary' 
+            classes={{
+              colorPrimary: classes.close
+            }}
+          >
+            <IconClose/>
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Button
+        variant='outlined'
+        color='primary'
+        onClick={handleSubmit}
+      >
 				Apply Filters
-			</Button>
-			<TextField
-				variant='outlined'
-				label='TREE ID'
-				placeholder='e.g. 80'
-				margin='normal'
-				InputLabelProps={{
-					shrink: true,
-				}}
-				value={treeId}
-				onChange={e => setTreeId(e.target.value)}
-			/>
-			<TextField
-				select
-				variant='outlined'
-				label='STATUS'
-				placeholder='e.g. 80'
-				value={status ? status : 'All'}
-				margin='normal'
-				InputLabelProps={{
-					shrink: true,
-				}}
-				onChange={e => setStatus(e.target.value === 'All'? '':e.target.value)}
-			>
-				{['All', 'Planted', 'Hole dug', 'Not a tree', 'Blurry'].map(name =>
-					<MenuItem
-						key={name}
-						value={name}
-					>
-						{name}
-					</MenuItem>
-				)} 
-			</TextField>
-			<FormControl
-				variant='outlined'
-				margin='normal'
-			>
-				<InputLabel
-					shrink={true}
-				>
+      </Button>
+      <Button
+        variant='outlined'
+        color='primary'
+        onclick={handleClear}
+      >
+        Clear Filters
+      </Button>
+      <TextField
+        variant='outlined'
+        label='TREE ID'
+        placeholder='e.g. 80'
+        margin='normal'
+        InputLabelProps={{
+          shrink: true
+        }}
+        value={treeId}
+        onChange={e => setTreeId(e.target.value)}
+      />
+      <TextField
+        select
+        variant='outlined'
+        label='STATUS'
+        placeholder='e.g. 80'
+        value={status ? status : 'All'}
+        margin='normal'
+        InputLabelProps={{
+          shrink: true
+        }}
+        onChange={e => setStatus(e.target.value === 'All' ? '' : e.target.value)}
+      >
+        {['All', 'Planted', 'Hole dug', 'Not a tree', 'Blurry'].map(name =>
+          <MenuItem
+            key={name}
+            value={name}
+          >
+            {name}
+          </MenuItem>
+        )} 
+      </TextField>
+      <FormControl
+        variant='outlined'
+        margin='normal'
+      >
+        <InputLabel
+          shrink={true}
+        >
 					TIME CREATED
-				</InputLabel>
-				<Grid 
-					container
-					justify='space-between'
-					style={{
-						marginTop: 19,
-					}}
-				>
-					<Grid item>
-						<OutlinedInput
-							type='date'
-							className={classes.dateInput}
-							value={dateStart}
-							onChange={handleDateStartChange}
-						/>
-					</Grid>
-					<Grid item>
-						<OutlinedInput
-							type='date'
-							className={classes.dateInput}
-							value={dateEnd}
-							onChange={handleDateEndChange}
-						/>
-					</Grid>
-				</Grid>
-			</FormControl>
-			
-		</Drawer>
-	)
+        </InputLabel>
+        <Grid 
+          container
+          justify='space-between'
+          style={{
+            marginTop: 19
+          }}
+        >
+          <Grid item>
+            <OutlinedInput
+              type='date'
+              className={classes.dateInput}
+              value={dateStart}
+              onChange={handleDateStartChange}
+            />
+          </Grid>
+          <Grid item>
+            <OutlinedInput
+              type='date'
+              className={classes.dateInput}
+              value={dateEnd}
+              onChange={handleDateEndChange}
+            />
+          </Grid>
+        </Grid>
+      </FormControl>
+    </Drawer>
+  )
 }
 
 export default compose(
