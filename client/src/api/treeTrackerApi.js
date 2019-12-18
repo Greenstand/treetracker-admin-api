9566e7b571d6ee46160c052acb4f5cbb29b18cac
Jay Moon <sjaymoon15@gmail.com>
2019-12-18 11:42:01 -0600

    Reverse order of trees to have oldest on the top

--- client/src/api/treeTrackerApi.js
+++ client/src/api/treeTrackerApi.js
@@ -2,83 +2,84 @@ import { handleResponse, handleError } from "./apiUtils";
 import { API_ROOT as baseUrl } from "../common/variables.js";
 
 export default {
-	getTreeImages({
-		skip,
-		rowsPerPage,
-		orderBy = "id",
-		order = "desc",
-		//the filter model
-		filter,
-	}) {
-		const query = `${baseUrl}/trees?` + 
-			`filter[order]=${orderBy} ${order}&` + 
-			`filter[limit]=${rowsPerPage}&` + 
-			`filter[skip]=${skip}&` + 
-			`filter[fields][imageUrl]=true&` + 
-			`filter[fields][lat]=true&` + 
-			`filter[fields][lon]=true&` + 
-			`filter[fields][id]=true&` + 
-			`filter[fields][timeCreated]=true&` + 
-			`filter[fields][timeUpdated]=true&` + 
-			`filter[fields][active]=true&` + 
-			`filter[fields][approved]=true&` +
-			`filter[fields][planterId]=true&` +
-			`filter[fields][deviceId]=true&` +
-			`filter[fields][planterIdentifier]=true&` +
-			`field[imageURL]` + 
-			//the filter query
-			filter.getBackloopString();
-		return fetch(query)
-			.then(handleResponse)
-			.catch(handleError);
-	},
-	approveTreeImage(id) {
-		const query = `${baseUrl}/trees/${id}`;
-		return fetch(query, {
-			method: "PATCH",
-			headers: { "content-type": "application/json" },
-			body: JSON.stringify({ 
-				id: id, 
-				approved: true ,
-				//revise, if click approved on a rejected pic, then, should set the pic
-				//approved, AND restore to ACTIVE = true
-				active		: true,
-			})
-		})
-			.then(handleResponse)
-			.catch(handleError);
-	},
-	rejectTreeImage(id) {
-		const query = `${baseUrl}/trees/${id}`;
-		return fetch(query, {
-			method: "PATCH",
-			headers: { "content-type": "application/json" },
-			body: JSON.stringify({ 
-				id: id,  
-				active: false ,
-				//revise, if click a approved pic, then, should set active = false and
-				//at the same time, should set approved to false
-				approved		: false,
-			})
-		})
-			.then(handleResponse)
-			.catch(handleError);
-	},
-	/* 
-	 * to rollback from a wrong approving
-	 */
-	undoTreeImage(id) {
-		const query = `${baseUrl}/trees/${id}`;
-		return fetch(query, {
-			method: "PATCH",
-			headers: { "content-type": "application/json" },
-			body: JSON.stringify({ 
-				id: id,  
-				active: true ,
-				approved		: false,
-			})
-		})
-			.then(handleResponse)
-			.catch(handleError);
-	}
-}
+  getTreeImages({
+    skip,
+    rowsPerPage,
+    orderBy = "id",
+    order = "asc",
+    //the filter model
+    filter
+  }) {
+    const query =
+      `${baseUrl}/trees?` +
+      `filter[order]=${orderBy} ${order}&` +
+      `filter[limit]=${rowsPerPage}&` +
+      `filter[skip]=${skip}&` +
+      `filter[fields][imageUrl]=true&` +
+      `filter[fields][lat]=true&` +
+      `filter[fields][lon]=true&` +
+      `filter[fields][id]=true&` +
+      `filter[fields][timeCreated]=true&` +
+      `filter[fields][timeUpdated]=true&` +
+      `filter[fields][active]=true&` +
+      `filter[fields][approved]=true&` +
+      `filter[fields][planterId]=true&` +
+      `filter[fields][deviceId]=true&` +
+      `filter[fields][planterIdentifier]=true&` +
+      `field[imageURL]` +
+      //the filter query
+      filter.getBackloopString();
+    return fetch(query)
+      .then(handleResponse)
+      .catch(handleError);
+  },
+  approveTreeImage(id) {
+    const query = `${baseUrl}/trees/${id}`;
+    return fetch(query, {
+      method: "PATCH",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        id: id,
+        approved: true,
+        //revise, if click approved on a rejected pic, then, should set the pic
+        //approved, AND restore to ACTIVE = true
+        active: true
+      })
+    })
+      .then(handleResponse)
+      .catch(handleError);
+  },
+  rejectTreeImage(id) {
+    const query = `${baseUrl}/trees/${id}`;
+    return fetch(query, {
+      method: "PATCH",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        id: id,
+        active: false,
+        //revise, if click a approved pic, then, should set active = false and
+        //at the same time, should set approved to false
+        approved: false
+      })
+    })
+      .then(handleResponse)
+      .catch(handleError);
+  },
+  /*
+   * to rollback from a wrong approving
+   */
+  undoTreeImage(id) {
+    const query = `${baseUrl}/trees/${id}`;
+    return fetch(query, {
+      method: "PATCH",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        id: id,
+        active: true,
+        approved: false
+      })
+    })
+      .then(handleResponse)
+      .catch(handleError);
+  }
+};
