d4fa01939cca50fbf69a8e2344f5c9cdba139574
lukasbicus <ing.lukas.bicus@gmail.com>
2020-05-16 17:01:53 +0200

    Revert reformatting of .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,30 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "jest": true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'react/prop-types': 'off',
-    'no-unused-vars': ['warn'],
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-  },
-}
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "react/prop-types": "off" ,
+      "no-unused-vars": ["warn"],
+    },
+    "settings": {
+      "react": {
+        "version": "detect",
+      }
+    }
+};
