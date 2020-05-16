75b225eb4664e886a76ddce1f547d3d03512cf83
lukasbicus <ing.lukas.bicus@gmail.com>
2020-05-16 14:43:50 +0200

    Add prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,35 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "jest": true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': ['warn'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-      "react/prop-types": "off" ,
-      "no-unused-vars": ["warn"],
-    },
-    "settings": {
-      "react": {
-        "version": "detect",
-      }
-    }
-};
+  },
+}
