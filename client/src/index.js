90c0aee9a38e7de1fd686c159fd77f5502c3d0ce
lukasbicus <ing.lukas.bicus@gmail.com>
2020-05-20 08:55:47 +0200

    revert prettier in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
+import './init'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import App from './App'
 import 'typeface-roboto'
 import { init } from '@rematch/core'
-import './init'
-import App from './App'
 import * as models from './models'
 import './index.css'
-import * as log from 'loglevel'
+import * as log from 'loglevel';
 
-log.info('init redux...')
-const store = init({ models })
+log.info('init redux...');
+const store = init({ models });
 
 ReactDOM.render(
   <Provider store={store}>
