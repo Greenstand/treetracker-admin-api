87775c31f4dd118f8131ce50c7ac8fbdaa887176
deanchen <antlet2004@gmail.com>
2020-05-28 08:43:02 +0800

    Added: can check permission by role;

--- server/src/js/auth.js
+++ server/src/js/auth.js
@@ -4,20 +4,43 @@ const jwt = require('jsonwebtoken');
 const jwtSecret =
   '3n398nrNMSI992298948)#€/#/€#€Q/H/DFN/FNF90DFNM(FN)(Fn)(FDN)(DNF)(æ';
 
+const ROLE_ADMIN = 0;
+
 const user = {
   username: "dadiorchen",
   firstName: "Dadior",
   lastName: "Chen",
+  password: "123456",
   role: [0, 1],
   email: "dadiorchen@outlook.com",
 }
 
+const userB = {
+  username: "bbb",
+  firstName: "B",
+  lastName: "B",
+  password: "123456",
+  role: [1],
+  email: "b@outlook.com",
+}
+
+const users = [user, userB];
+
 router.post('/login', async function login(req, res, next) {
   try {
     const {username, password} = req.body;
-    if (username === 'test' && password === 'password') {
+    const userLogin = users.reduce((a,c) => {
+      if(a){
+        return a;
+      }else if(c.username === username && c.password === password){
+        return c;
+      }else{
+        return a;
+      }
+    }, undefined);
+    if (userLogin) {
       //TODO get user
-      const token = await jwt.sign(user, jwtSecret);
+      const token = await jwt.sign(userLogin, jwtSecret);
       return res.json({
         token,
         user,
@@ -54,12 +77,25 @@ const isAuth = (req, res, next) => {
   try {
     const token = req.headers.authorization;
     const decodedToken = jwt.verify(token, jwtSecret);
-    const userId = decodedToken.userId;
+    const userSession = decodedToken;
 //    if (req.body.userId && req.body.userId !== userId) {
 //      throw new Error("Invalid user ID");
 //    } else {
-      req.userId = userId;
+      req.user = userSession;
+    //check role
+    //console.log("userB:", userSession);
+    if(req.baseUrl.match(/\/auth\/(?!login).*/)){
+      if(userSession.role.some(r => r === ROLE_ADMIN)){
+        next();
+        return;
+      }
+    }else{
       next();
+      return;
+    }
+    res.status(401).json({
+      error: new Error("No permission"),
+    });
       //res.status(200).json([user]);
   } catch {
     res.status(401).json({
--- server/src/js/auth.test.js
+++ server/src/js/auth.test.js
@@ -23,8 +23,8 @@ describe("auth", () => {
     const response = await request(app)
       .post("/auth/login")
       .send({
-        username: "test",
-        password: "password",
+        username: "dadiorchen",
+        password: "123456",
       });
     expect(response.statusCode).toBe(200);
     expect(response.body).toBeDefined();
@@ -48,8 +48,8 @@ describe("auth", () => {
       const response = await request(app)
         .post("/auth/login")
         .send({
-          username: "test",
-          password: "password",
+          username: "dadiorchen",
+          password: "123456",
         });
       expect(response.statusCode).toBe(200);
       expect(response.body).toBeDefined();
@@ -62,6 +62,32 @@ describe("auth", () => {
         .get("/auth/admin_users")
         .set("Authorization", token);
       expect(res.statusCode).toBe(200);
+      expect(res.body).toBeInstanceOf(Array);
+    });
+
+  });
+
+  describe("Login B", () => {
+    let token;
+
+    beforeEach(async () => {
+      const response = await request(app)
+        .post("/auth/login")
+        .send({
+          username: "bbb",
+          password: "123456",
+        });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toBeDefined();
+      expect(response.body.token).toMatch(/\S+/);
+      token = response.body.token;
+    });
+
+    it("admin_users 401", async () => {
+      const res = await request(app)
+        .get("/auth/admin_users")
+        .set("Authorization", token);
+      expect(res.statusCode).toBe(401);
     });
 
   });
