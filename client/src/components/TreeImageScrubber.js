8d6254e1030efc6b5cd10c955d4f11d731cf3b97
Eric <eric_vogt_@hotmail.com>
2020-08-08 16:14:44 -0600

    TreeImageScrubber Capture Types Corrected

--- client/src/components/TreeImageScrubber.js
+++ client/src/components/TreeImageScrubber.js
@@ -579,7 +579,7 @@ function SidePanel(props){
   const [switchApprove, handleSwitchApprove] = React.useState(0)
   const [morphology, handleMorphology] = React.useState('seedling')
   const [age, handleAge] = React.useState('new_tree')
-  const [captureApprovalTag, handleCaptureApprovalTag] = React.useState('simple_lead')
+  const [captureApprovalTag, handleCaptureApprovalTag] = React.useState('simple_leaf')
   const [rejectionReason, handleRejectionReason] = React.useState('not_tree')
   const speciesRef = React.useRef(null)
 
@@ -678,8 +678,8 @@ function SidePanel(props){
               value={captureApprovalTag}
             >
               <FormControlLabel 
-                onClick={() => handleCaptureApprovalTag('simple_lead')}
-                value='simple_lead' control={<Radio/>} label='Simple leaf' />
+                onClick={() => handleCaptureApprovalTag('simple_leaf')}
+                value='simple_leaf' control={<Radio/>} label='Simple leaf' />
               <FormControlLabel 
                 onClick={() => handleCaptureApprovalTag('complex_leaf')}
                 value='complex_leaf' control={<Radio/>} label='Complex leaf' />
@@ -696,8 +696,8 @@ function SidePanel(props){
                 onClick={() => handleCaptureApprovalTag('mangrove')}
                 value='mangrove' control={<Radio/>} label='Mangrove' />
               <FormControlLabel 
-                onClick={() => handleCaptureApprovalTag('plam')}
-                value='plam' control={<Radio/>} label='Palm' />
+                onClick={() => handleCaptureApprovalTag('palm')}
+                value='palm' control={<Radio/>} label='Palm' />
               <FormControlLabel 
                 onClick={() => handleCaptureApprovalTag('timber')}
                 value='timber' control={<Radio/>} label='Timber' />
