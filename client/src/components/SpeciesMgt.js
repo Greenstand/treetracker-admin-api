e86cafa85db253154a1e3bd491c33ca68fcdd2c3
Elfi Yang <elfiyang16@gmail.com>
2020-08-13 14:55:59 +0100

    refactor: clean up

--- client/src/components/SpeciesMgt.js
+++ client/src/components/SpeciesMgt.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import SpecieTable from './SpecieTable'
-import Navbar from './Navbar'
 import { Grid } from '@material-ui/core'
 
 const SpeciesMgt = () => {
   return (
     <Grid container direction="column" style={{ flexWrap: 'nowrap', height: '100%' }}>
-      <Grid item>
-        <Navbar />
-      </Grid>
       <Grid item container style={{ height: '100%', overflow: 'hidden' }}>
         <SpecieTable />
       </Grid>
