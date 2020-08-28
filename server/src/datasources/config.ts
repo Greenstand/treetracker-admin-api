1e453db85698906499f0551bd15033918bd18588
deanchen <antlet2004@gmail.com>
2020-08-28 15:37:40 +0800

    fxied: import problem for test db

--- server/src/datasources/config.ts
+++ server/src/datasources/config.ts
@@ -1,5 +1,5 @@
-import json from "./treetracker.datasource.json";
-import jsonTest from "./treetrackerTest.datasource.json";
+const json = require("./treetracker.datasource.json");
+const jsonTest = require("./treetrackerTest.datasource.json");
 
 function getDatasource(){
   const config = process.env.NODE_DB === "test" ?
