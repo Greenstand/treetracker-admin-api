511b985633f220717509238b8d9d368e7a193d32
uchan3 <urey.chan3@gmail.com>
2020-08-29 15:41:39 -0600

    Add passing test with env variables

---
+++ server/src/__tests__/species.controller.ts
@@ -0,0 +1,28 @@
+import {expect} from '@loopback/testlab'
+import { TreetrackerDataSource } from '../datasources'
+import { SpeciesController } from '../controllers';
+import { SpeciesRepository } from '../repositories';
+require('dotenv').config();
+
+describe('Species Controller Integration Tests', () => {
+    //Configure the database with env. variables. 
+    const config = 
+    {
+        name: process.env.DB_NAME, 
+        connector: process.env.DB_CONNECTOR, 
+        url: process.env.DB_URL, 
+        port: process.env.DB_PORT, 
+    };      
+    const datasource = new TreetrackerDataSource(config);
+
+    describe('Count number of species', () => {
+        it('return total species', async () => {
+            const expect_Result: number = 1;
+            const controller = new SpeciesController(new SpeciesRepository(datasource)); 
+            const count_Species = (await controller.count()).count; 
+            //console.log(details); //Debug
+            expect(count_Species).equal(expect_Result);
+        });
+    });
+});
+
