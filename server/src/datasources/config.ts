8cf06ab8b6ebe1ff1dfb086c86cb2c07074d2f09
deanchen <antlet2004@gmail.com>
2020-09-22 06:58:55 +0800

    Added: check if the NODE_DB was set if it is test (Jest)

--- server/src/datasources/config.ts
+++ server/src/datasources/config.ts
@@ -9,6 +9,11 @@ export interface DatasourceConfig {
   database: string;
 }
 
+//check the test env, if this is testing, then the NODE_DB=test must be set
+if(process.env.NODE_ENV === 'test' && process.env.NODE_DB !== 'test' ){
+  throw new Error("This is test env, please set NODE_DB === 'test'");
+}
+
 const config: DatasourceConfig =
   process.env.NODE_DB === 'test'
     ? require('./treetrackerTest.datasource.json')
