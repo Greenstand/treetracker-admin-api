8b28fe424217412e87ef78907f4f879185d5218c
Nick Charlton <nmcharlton@hotmail.com>
2020-12-05 17:52:23 +0000

    Remove redundant comment

--- server/src/models/planterRegistration.model.ts
+++ server/src/models/planterRegistration.model.ts
@@ -42,8 +42,6 @@ export class PlanterRegistration extends Entity {
   })
   createdAt: string;
 
-  // Define well-known properties here
-
   // Indexer property to allow additional data
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [prop: string]: any;
