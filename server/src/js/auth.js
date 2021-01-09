0176b9e78b8ba09a56be5bafc425d99a99d6efc5
Nick Charlton <nmcharlton@hotmail.com>
2021-01-09 11:58:17 +0000

    fix(server): move auth helper fn to auth.js

--- server/src/js/auth.js
+++ server/src/js/auth.js
@@ -31,6 +31,20 @@ const POLICIES = {
   MANAGE_PLANTER: 'manage_planter',
 };
 
+helper.needRoleUpdate = function(update_userSession, userSession) {
+  /* if no role exists prev/current, then update regardless and don't check if both are equal */
+  const hasRoles = !!update_userSession.role && !!userSession.role;
+  if (!hasRoles) {
+    return true;
+  } else {
+    /*check if roles equal */
+    const isEqual =
+      update_userSession.role.length === userSession.role.length &&
+      userSession.role.every((val) => update_userSession.role.includes(val));
+    return isEqual ? false : true;
+  }
+};
+
 helper.sha512 = function (password, salt) {
   const hash = Crypto.createHmac('sha512', salt);
   hash.update(password);
