a304e10804d4f88151bc26398732c0d361c6b58c
Nick Charlton <nmcharlton@hotmail.com>
2021-01-24 21:14:35 +0000

    feat: add extra test for planter images

--- client/src/components/EditPlanter.spec.py.js
+++ client/src/components/EditPlanter.spec.py.js
@@ -41,17 +41,24 @@ describe('EditPlanter', () => {
 
     const planter = {
       id: 12345,
-      imageUrl: 'https://greenstand.org/fileadmin/_processed_/f/e/csm_MVIMG_20200303_103438_e3a293cbe2.jpg',
+      imageUrl: 'https://greenstand.org/fileadmin/_processed_/f/e/csm_MVIMG_20200303_103438_be16bc7f80.jpg',
       firstName: 'Teston',
       lastName: 'Blumenfail',
       email: 'test@email.com',
       phone: '+1234567890'
     }
 
+    let planterSelfies
+
     beforeEach(() =>{
-      cy.stub(api, 'getPlanterSelfies').returns([
-        'https://greenstand.org/fileadmin/_processed_/9/f/csm_2019.08.12.09.54.39_1ca43554-b139-4ae2-bbc9-c9a37c43e645_IMG_20190812_093641_-1471408775_56b4574917.jpg'
-      ])
+      planterSelfies = [
+        'https://greenstand.org/fileadmin/_processed_/d/4/csm_little_Jony_bdf756638d.jpg',
+        'https://greenstand.org/fileadmin/_processed_/9/f/csm_2019.08.12.09.54.39_1ca43554-b139-4ae2-bbc9-c9a37c43e645_IMG_20190812_093641_-1471408775_0bb24d7c21.jpg',
+        'https://greenstand.org/fileadmin/_processed_/e/3/csm_IMG_0017_3c859de144.jpg',
+        'https://greenstand.org/fileadmin/_processed_/9/3/csm_PHOTO-2019-08-05-11-50-37_f0d0281499.jpg',
+      ]
+
+      cy.stub(api, 'getPlanterSelfies').returns(planterSelfies)
       mount(
         <Provider store={store}>
           <ThemeProvider theme={theme}>
@@ -62,11 +69,17 @@ describe('EditPlanter', () => {
     })
 
     it('should display planter details', () => {
-      cy.get('input#firstName').should('have.value', planter.firstName)
+      cy.get(`[title="${planter.imageUrl}"]`).should('have.css', 'background-image', `url("${planter.imageUrl}")`)
       cy.get('input#firstName').should('have.value', planter.firstName)
       cy.get('input#lastName').should('have.value', planter.lastName)
       cy.get('input#email').should('have.value', planter.email)
       cy.get('input#phone').should('have.value', planter.phone)
+    })
+
+    it('should display all other planter images', () => {
+      planterSelfies.forEach((img) => {
+        cy.get(`[title="${img}"]`).should('have.css', 'background-image', `url("${img}")`)
+      })
     })
 
     it('should enable Save button when values change', () => {
