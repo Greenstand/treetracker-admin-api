72314a4d74a05c13e0c64fd5dd8410d621584376
Nick Charlton <nmcharlton@hotmail.com>
2021-03-09 22:44:24 +0000

    fix: build composite set of policies from roles

--- src/js/auth.js
+++ src/js/auth.js
@@ -7,14 +7,14 @@ import generator from 'generate-password';
 import Crypto from 'crypto';
 import bodyParser from 'body-parser';
 import { config } from '../config';
-import {Pool} from 'pg';
-import {utils} from './utils';
+import { Pool } from 'pg';
+import { utils } from './utils';
 import getDatasource from '../datasources/config';
 import policy from '../policy.json';
 import expect from 'expect';
 
 const app = express();
-const pool = new Pool({connectionString: getDatasource().url});
+const pool = new Pool({ connectionString: getDatasource().url });
 const jwtSecret = config.jwtSecret;
 
 //collect all those functions who visit DB into a variables, to give some convenience
@@ -31,7 +31,7 @@ const POLICIES = {
   MANAGE_PLANTER: 'manage_planter',
 };
 
-helper.needRoleUpdate = function(update_userSession, userSession) {
+helper.needRoleUpdate = function (update_userSession, userSession) {
   /* if no role exists prev/current, then update regardless and don't check if both are equal */
   const hasRoles = !!update_userSession.role && !!userSession.role;
   if (!hasRoles) {
@@ -53,11 +53,11 @@ helper.sha512 = function (password, salt) {
 };
 
 const generateSalt = function () {
-  const generated = generator.generate({length: 6, numbers: true});
+  const generated = generator.generate({ length: 6, numbers: true });
   return generated;
 };
 
-const jsonParser = app.use(bodyParser.urlencoded({extended: false})); // parse application/json
+const jsonParser = app.use(bodyParser.urlencoded({ extended: false })); // parse application/json
 // const urlencodedParser = app.use(bodyParser.json());/// parse application/x-www-form-urlencoded
 
 function isIdValid(id) {
@@ -110,19 +110,34 @@ helper.deactivateAdminUser = async function (userId) {
 
 // load roles and policy (permissions)
 helper.loadUserPermissions = async function (userId) {
-    const userDetails = {};
-    let result;
-    //get role
-    result = await helper.getActiveAdminUserRoles(userId);
-    expect(result.rows.length).toBeGreaterThan(0);
-    userDetails.role = result.rows.map(r => r.role_id);
-    //get policies
-    result = await pool.query(
-      `select * from admin_role where id = ${userDetails.role[0]}`,
-    );
-    userDetails.policy = result.rows.map(r => r.policy)[0];
-    return userDetails;
+  const userDetails = {};
+  //get roles
+  const result = await helper.getActiveAdminUserRoles(userId);
+  expect(result.rows.length).toBeGreaterThan(0);
+  userDetails.role = result.rows.map(r => r.role_id);
+  //get policies
+  const policyObjects = await Promise.all(userDetails.role.map(async (role) => {
+    const res = await pool.query(`select * from admin_role where id = ${role}`);
+    return res.rows.map(r => r.policy)[0];
+  }));
+
+  // Build a composite policy object from the various roles
+  userDetails.policy = policyObjects.reduce((compositePolicy, policyObj) => {
+    // Assume one organization and apply to all policies
+    if (policyObj.organization) {
+      compositePolicy.organization = policyObj.organization;
+    }
+    policyObj.policies.forEach(p => {
+      if (!compositePolicy.policies.find((cp) => cp.name === p.name)) {
+        compositePolicy.policies.push(p);
+      }
+    });
+
+    return compositePolicy;
+  }, { policies: [] });
+  return userDetails;
 }
+
 
 router.get('/permissions', async function login(req, res) {
   try {
@@ -138,7 +153,7 @@ router.post('/login', async function login(req, res, next) {
   try {
     //try to init, in case of first visit
     // await init();
-    const {userName, password} = req.body;
+    const { userName, password } = req.body;
 
     //find the user to get the salt, validate if hashed password matches
     const users = await helper.getActiveAdminUser(userName);
@@ -154,7 +169,7 @@ router.post('/login', async function login(req, res, next) {
         //console.assert(userLogin.id >= 0, 'id?', userLogin);
         const result = await helper.getActiveAdminUserRoles(userLogin.id)
         userLogin.role = result.rows.map(r => r.role_id);
-      }else{
+      } else {
         console.log("checking password failed");
       }
     } else {
@@ -165,7 +180,7 @@ router.post('/login', async function login(req, res, next) {
     // query remaining details and return
     if (userLogin && userLogin.enabled) {
       const userDetails = await helper.loadUserPermissions(userLogin.id);
-      userLogin = {...userLogin, ...userDetails};
+      userLogin = { ...userLogin, ...userDetails };
       //TODO get user
       const token = await jwt.sign(userLogin, jwtSecret);
       const {
@@ -180,7 +195,7 @@ router.post('/login', async function login(req, res, next) {
       console.log('login success');
       return res.json({
         token,
-        user: {id, userName, firstName, lastName, email, role, policy},
+        user: { id, userName, firstName, lastName, email, role, policy },
       });
     } else {
       console.log('login failed:', userLogin);
@@ -207,7 +222,7 @@ router.get('/admin_users/:userId', async (req, res) => {
     let userGet;
     if (result.rows.length === 1) {
       userGet = utils.convertCamel(result.rows[0]);
-      //load role
+      //load roles
       result = await helper.getActiveAdminUserRoles(userGet.id);
       userGet.role = result.rows.map(r => r.role_id);
     }
@@ -291,7 +306,7 @@ router.get('/admin_users/', async (req, res) => {
 
 router.post('/validate/', async (req, res) => {
   try {
-    const {password} = req.body;
+    const { password } = req.body;
     const token = req.headers.authorization;
     const decodedToken = jwt.verify(token, jwtSecret);
     const userSession = decodedToken;
@@ -320,7 +335,7 @@ router.post('/admin_users/', async (req, res) => {
     let result = await helper.getActiveAdminUser(req.body.userName)
     if (result.rows.length) {
       //TODO 401
-      res.status(201).json({id: result.rows[0].id});
+      res.status(201).json({ id: result.rows[0].id });
       return;
     }
     //active
@@ -340,16 +355,14 @@ router.post('/admin_users/', async (req, res) => {
     let obj;
     if (result.rows.length) {
       obj = result.rows[0];
-      //roles
-      //role
       await helper.clearAdminUserRoles(obj.id)
-      for (let i = 0; i < req.body.role.length; i++) {
-        await helper.addAdminUserRole(obj.id, req.body.role[i])
+      for (const role of req.body.role) {
+        await helper.addAdminUserRole(obj.id, role)
       }
     } else {
       throw new Error('can not find new user');
     }
-    res.status(201).json({id: obj.id});
+    res.status(201).json({ id: obj.id });
   } catch (e) {
     console.error(e);
     res.status(500).json();
@@ -371,29 +384,29 @@ async function init() {
 
   await pool.query(
     `insert into admin_role (id, role_name, description, policy) ` +
-      `values (1, 'Admin', 'The super administrator role, having all permissions','${JSON.stringify(
-        [policy.policies[0], policy.policies[1], policy.policies[2]],
-      )}'),` +
-      `(2, 'Tree Manager', 'Check, verify, manage trees','${JSON.stringify([
-        policy.policies[3],
-        policy.policies[4],
-      ])}'),` +
-      `(3, 'Planter Manager', 'Check, manage planters','${JSON.stringify([
-        policy.policies[5],
-        policy.policies[6],
-      ])}')`,
+    `values (1, 'Admin', 'The super administrator role, having all permissions','${JSON.stringify(
+      [policy.policies[0], policy.policies[1], policy.policies[2]],
+    )}'),` +
+    `(2, 'Tree Manager', 'Check, verify, manage trees','${JSON.stringify([
+      policy.policies[3],
+      policy.policies[4],
+    ])}'),` +
+    `(3, 'Planter Manager', 'Check, manage planters','${JSON.stringify([
+      policy.policies[5],
+      policy.policies[6],
+    ])}')`,
   );
 
   await pool.query(
     `insert into admin_user (id, user_name, first_name, last_name, password_hash, salt, email, active) ` +
-      `values ( 1, 'admin', 'Admin', 'Panel', 'eab8461725c44aa1532ed88de947fe0706c00c31ed6d832218a6cf59d7602559a7d372d42a64130f21f1f33091105548514bca805b81ee1f01a068a7b0fa2d80', 'OglBTs','admin@greenstand.org', true),` +
-      `(2, 'test', 'Admin', 'Test', '539430ec2a48fd607b6e06f3c3a7d3f9b46ac5acb7e81b2633678a8fe3ce6216e2abdfa2bc41bbaa438ba55e5149efb7ad522825d9e98df5300b801c7f8d2c86', 'WjSO0T','test@greenstand.org', true)`,
+    `values ( 1, 'admin', 'Admin', 'Panel', 'eab8461725c44aa1532ed88de947fe0706c00c31ed6d832218a6cf59d7602559a7d372d42a64130f21f1f33091105548514bca805b81ee1f01a068a7b0fa2d80', 'OglBTs','admin@greenstand.org', true),` +
+    `(2, 'test', 'Admin', 'Test', '539430ec2a48fd607b6e06f3c3a7d3f9b46ac5acb7e81b2633678a8fe3ce6216e2abdfa2bc41bbaa438ba55e5149efb7ad522825d9e98df5300b801c7f8d2c86', 'WjSO0T','test@greenstand.org', true)`,
   );
   await pool.query(
     `insert into admin_user_role (id, role_id, admin_user_id, active) ` +
-      `values ( 1, 1, 1, true), ` +
-      `(2, 2, 2, true), ` +
-      `(3, 3, 2, true)`,
+    `values ( 1, 1, 1, true), ` +
+    `(2, 2, 2, true), ` +
+    `(3, 3, 2, true)`,
   );
 }
 
@@ -456,13 +469,13 @@ const isAuth = async (req, res, next) => {
         //compare wuth the updated pwd in case pwd is changed
         if (update_userSession.passwordHash === userSession.passwordHash) {
           /*get the role for updated usersession* */
-          const updated_role = await helper.getActiveAdminUserRoles(user_id);
-          update_userSession.role = updated_role.rows.map(r => r.role_id);
+          const updated_roles = await helper.getActiveAdminUserRoles(user_id);
+          update_userSession.role = updated_roles.rows.map(r => r.role_id);
           //compare wuth the updated role in case role is changed
           if (helper.needRoleUpdate(update_userSession, userSession)) {
-            //reassign token with updated role if role changes
+            //reassign token with updated roles if roles change
             const new_token = jwt.sign(update_userSession, jwtSecret);
-            res.status(200).json({token: new_token});
+            res.status(200).json({ token: new_token });
             return;
           } else {
             console.log('auth check');
@@ -480,11 +493,6 @@ const isAuth = async (req, res, next) => {
       }
     }
     if (url.match(/\/auth\/(?!login).*/)) {
-      //if role is admin, then can do auth stuff
-      // if (userSession.role.some(r => r === PERMISSIONS.ADMIN)) {
-      //   next();
-      //   return;
-      // }
       if (policies.some((r) => r.name === POLICIES.SUPER_PERMISSION)) {
         next();
         return;
