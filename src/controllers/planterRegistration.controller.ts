669d8510f8d4d0995fcb9bc4562fa2f5f2fea407
Carlos Martins <luacmartins@gmail.com>
2021-03-12 12:56:26 -0700

    fix: abstract buildFilterQuery

--- src/controllers/planterRegistration.controller.ts
+++ src/controllers/planterRegistration.controller.ts
@@ -1,8 +1,8 @@
 import { Filter, repository, Where } from '@loopback/repository';
-import { ParameterizedSQL } from 'loopback-connector';
 import { param, get, getFilterSchemaFor } from '@loopback/rest';
-import { Planter, PlanterRegistration } from '../models';
+import { PlanterRegistration } from '../models';
 import { PlanterRegistrationRepository } from '../repositories';
+import { buildFilterQuery } from '../js/buildFilterQuery.js';
 
 // Extend the LoopBack filter types for the Trees model to include tagId
 // This is a workaround for the lack of proper join support in LoopBack
@@ -38,15 +38,19 @@ export class PlanterRegistrationController {
     @param.query.object('filter', getFilterSchemaFor(PlanterRegistration))
     filter?: PlanterRegistrationFilter,
   ): Promise<PlanterRegistration[]> {
-    const query = this.buildFilterQuery(
-      `SELECT * FROM planter_registrations`,
-      `LEFT JOIN (
+    const sql = `SELECT * FROM planter_registrations 
+        LEFT JOIN (
         SELECT region.name AS country, region.geom FROM region, region_type
         WHERE region_type.type='country' AND region.type_id=region_type.id
-    ) AS region ON ST_DWithin(region.geom, planter_registrations.geom, 0.01)`,
-      '',
-      filter?.where,
-    );
+    ) AS region ON ST_DWithin(region.geom, planter_registrations.geom, 0.01)`;
+
+    const params = {
+      filter: filter?.where,
+      repo: this.planterRepository,
+      model: 'PlanterRegistration',
+    };
+
+    const query = buildFilterQuery(sql, params);
 
     return <Promise<PlanterRegistration[]>>(
       await this.planterRepository.execute(query.sql, query.params)
@@ -67,55 +71,5 @@ export class PlanterRegistrationController {
     @param.path.number('id') id: number,
   ): Promise<PlanterRegistration> {
     return await this.planterRepository.findById(id);
-  }
-
-  private getConnector() {
-    return this.planterRepository.dataSource.connector;
-  }
-
-  private buildFilterQuery(
-    selectClause: string,
-    joinClause?: string,
-    whereClause?: string,
-    whereObj?: PlanterRegistrationWhere,
-  ) {
-    let sql = selectClause;
-    if (joinClause) {
-      sql += ` ${joinClause}`;
-    }
-
-    if (whereClause) {
-      sql += ` ${whereClause}`;
-    }
-
-    let query = new ParameterizedSQL(sql);
-
-    if (whereObj) {
-      const connector = this.getConnector();
-      if (connector) {
-        const model = connector._models.PlanterRegistration.model;
-
-        if (model) {
-          let safeWhere = model._sanitizeQuery(whereObj);
-          safeWhere = model._coerce(safeWhere);
-
-          const whereObjClause = connector._buildWhere(
-            'PlanterRegistration',
-            safeWhere,
-          );
-          console.log(whereObjClause);
-          if (whereObjClause && whereObjClause.sql) {
-            query.sql += ` ${whereClause ? 'AND' : 'WHERE'} ${
-              whereObjClause.sql
-            }`;
-            query.params = whereObjClause.params;
-          }
-
-          query = connector.parameterize(query);
-        }
-      }
-    }
-
-    return query;
   }
 }
