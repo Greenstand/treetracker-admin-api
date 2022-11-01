0c42e194d38500b9ddd3f1a1f7c152c8e1e1f927
kothariroshni8 <51296552+kothariroshni8@users.noreply.github.com>
2022-11-01 13:44:09 -0700

    feat(species):capture count if user has permission (#645)
    * feat(species):capture count if user has permission
    
    * feat(species): capture count if user has permission
    
    * feat(species): capture count if user has permission

--- src/controllers/species.controller.ts
+++ src/controllers/species.controller.ts
@@ -1,3 +1,4 @@
+import { inject } from '@loopback/context';
 import {
   Count,
   CountSchema,
@@ -16,8 +17,10 @@ import {
   del,
   requestBody,
 } from '@loopback/rest';
+import { Request, RestBindings } from '@loopback/rest';
 import { Species } from '../models';
 import { SpeciesRepository, TreesRepository } from '../repositories';
+const ALLOWED_POLICIES = ['super_permission'];
 
 export class SpeciesController {
   constructor(
@@ -25,6 +28,8 @@ export class SpeciesController {
     public speciesRepository: SpeciesRepository,
     @repository(TreesRepository)
     public treesRepository: TreesRepository,
+    @inject(RestBindings.Http.REQUEST)
+    private request: Request,
   ) {}
 
   @get('/species/count', {
@@ -61,19 +66,37 @@ export class SpeciesController {
     filter?: Filter<Species> & { fields?: { captureCount?: Boolean } },
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ): Promise<any[]> {
-    console.log('get /species filter --> ', filter ? filter.where : null);
     // Only include active species
     filter = { ...filter, where: { ...filter?.where, active: true } };
+    const speciesList = await this.speciesRepository.find(filter);
 
+    if (this.requestHasUser(this.request)) {
+      const userPolicies: any[] = this.request.user.policy.policies;
+      const isAllowedUser = userPolicies.some((userPolicy) =>
+        ALLOWED_POLICIES.some(
+          (allowedPolicy) => allowedPolicy == userPolicy.name,
+        ),
+      );
+      if (isAllowedUser) {
+        return this.captureCountForEachSpecies(speciesList);
+      }
+    }
+    return speciesList;
+    // TODO: combine both queries in one query to minimise overhead
+  }
+
+  requestHasUser(request: Request): request is Request & { user: any } {
+    return 'user' in request;
+  }
+
+  async captureCountForEachSpecies(speciesList: Species[]): Promise<any[]> {
     const captureCountStmt =
       `SELECT species_id,COUNT(species_id) as count FROM trees ` +
       `WHERE species_id NOTNULL AND active=true GROUP BY species_id`;
-    // TODO: combine both queries in one query to minimise overhead
-    const [speciesList, captureCounts] = await Promise.all([
-      this.speciesRepository.find(filter),
-      this.treesRepository.execute(captureCountStmt, []),
-    ]);
-
+    const captureCounts = await this.treesRepository.execute(
+      captureCountStmt,
+      [],
+    );
     // Merge the capture counts with the species list
     return speciesList.map((species) => {
       const match = captureCounts.find((item) => {
